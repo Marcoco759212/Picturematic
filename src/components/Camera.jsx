f96056c1671033993ebfcebd01655a5d15dbd8b9
Marcoco759212 <marcoco.mahr@gmail.com>
2022-11-01 10:09:43 -0600

    Primera version de la app de fotos | la finalidad es probar la camara de distintos dispositivos (part4)

--- src/components/Camera.jsx
+++ src/components/Camera.jsx
@@ -1,74 +1,86 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
 const Camera = () => {
-
-    let videoRef = useRef(null);
- 
-    let photoRef = useRef(null);
-   
+    const videoRef = useRef(null);
+    const photoRef = useRef(null);
+    const stripRef = useRef(null);
+    const colorRef = useRef(null);
+  
+    useEffect(() => {
+      getVideo();
+    }, [videoRef]);
+  
     const getVideo = () => {
       navigator.mediaDevices
-        .getUserMedia({
-            video: true
-        })
-        .then((stream) => {
+        .getUserMedia({ video: { width: 780 } })
+        .then(stream => {
           let video = videoRef.current;
           video.srcObject = stream;
           video.play();
         })
-        .catch((err) => {
-          console.error(err);
+        .catch(err => {
+          console.error("error:", err);
         });
-    }
-   
-    const takePicture = () => {
-        // const width = 400;
-        const width = 400;
-        // const height = width / (16 / 9);
-        const height = 400;
-        console.log('videooo', videoRef.current);
-        console.log('photooo', photoRef.current);
-        let video = videoRef.current;
-        let photo = photoRef.current;
-        video.width = width;
-        video.height = height;
-        let ctx = photo.getContext('2d');
+    };
+  
+    const paintToCanvas = () => {
+      let video = videoRef.current;
+      let photo = photoRef.current;
+      let ctx = photo.getContext("2d");
+  
+      const width = 320;
+      const height = 240;
+      photo.width = width;
+      photo.height = height;
+  
+      return setInterval(() => {
+        let color = colorRef.current;
+  
         ctx.drawImage(video, 0, 0, width, height);
-    }
-   
-    // const clearImage = () => {
-    //     let photo = photoRef.current;
-    //     let ctx = photo.getContext('2d');
-    //     ctx.clearRect(0,0,photo.width,photo.height);
-    // }
-
-    useEffect(() => {
-        return() => {
-            console.log('start camera');
-            getVideo();
-        }
-    }, [videoRef]);
-
+        let pixels = ctx.getImageData(0, 0, width, height);
+  
+        // color.style.backgroundColor = `rgb(${pixels.data[0]},${pixels.data[1]},${
+        //   pixels.data[2]
+        // })`;
+        // color.style.borderColor = `rgb(${pixels.data[0]},${pixels.data[1]},${
+        //   pixels.data[2]
+        // })`;
+      }, 200);
+    };
+  
+    const takePhoto = () => {
+      let photo = photoRef.current;
+      let strip = stripRef.current;
+  
+      const data = photo.toDataURL("image/jpeg");
+  
+      console.warn(data);
+      const link = document.createElement("a");
+      link.href = data;
+      link.setAttribute("download", "myWebcam");
+      link.innerHTML = `<img src='${data}' alt='thumbnail'/>`;
+      strip.insertBefore(link, strip.firstChild);
+    };
+  
     return (
-        <div className='camera-content'>
-            <video ref={videoRef} 
-                className="camera-image">
-            </video>
-            <div className='camera-content-images'>
-                <canvas className="camera-galery" 
-                    ref={photoRef}>
-                </canvas>
-            </div>
-            <button onClick={takePicture} 
-                className="camera-btn">
-            </button>
+        <div className="container">
+        {/* <div ref={colorRef} className="scene">
 
-            {/* <button onClick={clearImage} 
-                className="btn btn-primary container">
-                Clear Image
-            </button> */}
+        </div> */}
+            <div className="webcam-video">
+                <button onClick={() => takePhoto()}>Comenzar</button>
+                <video
+                    onCanPlay={() => paintToCanvas()}
+                    ref={videoRef}
+                    className="player"
+                />
+                <canvas ref={photoRef} className="photo" />
+                <div className="photo-booth">
+                    <div ref={stripRef} className="strip" />
+                </div>
+            </div>
         </div>
     );
-}
-
+  };
+  
 export default Camera;
